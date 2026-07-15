002deb30c1cbe8938d0f0969bcc3320aa1117ae9
Swarnim Rijal <swarnimisgone@gmail.com>
2026-07-15 16:44:28 +0545

    Added object list to Scene

--- src/game/Scene.java
+++ src/game/Scene.java
@@ -1,12 +1,44 @@
 package game;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.GameObject;
 
 public abstract class Scene {
 
+     protected final List<GameObject> gameObjects = new ArrayList<>();
+
+     // Adds object to the list
+     public void add(GameObject gameObject) {
+          if(gameObject == null) {
+               throw new IllegalArgumentException("Object can't be null");
+          }
+
+          gameObjects.add(gameObject);
+     }
+     
+     // Removes object from the list
+     public void remove(GameObject gameObject) {
+          if(gameObject == null) {
+               throw new IllegalArgumentException("Object can't be null");
+          }
+
+               gameObjects.remove(gameObject);
+     }
+
      // Updates the scene logic every frame
-     public abstract void update();
+     public void update() {
+          for(GameObject obj : gameObjects) {
+               obj.update();
+          }
+     }
 
      // Draws the scene to the screen
-     public abstract void render(Graphics2D g);
+     public void render(Graphics2D g) {
+          for(GameObject obj : gameObjects) {
+               obj.render(g);
+          }
+     }
 }
