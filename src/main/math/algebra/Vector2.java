1ff9a6950ac90e118a673a64278c9021451d557f
Swarnim Rijal <swarnimisgone@gmail.com>
2026-08-01 12:37:10 +0545

    Retracked files

---
+++ src/main/math/algebra/Vector2.java
@@ -0,0 +1,185 @@
+package main.math.algebra;
+
+// Class representing vector quantites (positions, velocity etc..)
+public class Vector2 {
+     private final float x;
+     private final float y;
+
+     public static final Vector2 ZERO = new Vector2(0, 0);
+
+     // Standard unit vectors
+     public static final Vector2 UP = new Vector2(0, 1);
+     public static final Vector2 DOWN = new Vector2(0, -1);
+     public static final Vector2 RIGHT = new Vector2(1, 0);
+     public static final Vector2 LEFT = new Vector2(-1, 0);
+
+     public Vector2(float x, float y) {
+          this.x = x;
+          this.y = y;
+     }
+
+     // Main getters
+     public float getX() {
+          return x;
+     }
+
+     public float getY() {
+          return y;
+     }
+
+     // Returns the dot product between vectors 
+     public float dot(Vector2 v2) {
+          return x * v2.getX() + y * v2.getY();
+     }
+
+     // Returns the 2D cross prodcut
+     public float cross(Vector2 v2) {
+          return x * v2.getY() - y * v2.getX();
+     }
+
+     // Returns the angle of the vector
+     public float getAngle() {
+          return (float) Math.atan2(y, x);
+     }
+
+     // Returns the magnitude of a vector
+     public float getMagnitude() {
+          float h = x * x + y * y;
+          return (float) Math.sqrt(h);
+     }
+     
+     // Adds the vector
+     public Vector2 add(Vector2 v2) {
+          Vector2 v = new Vector2(
+               x + v2.getX(),
+               y + v2.getY()
+          );
+
+          return v;
+     }
+
+     // Subtracts the vector 
+     public Vector2 subtract(Vector2 v2) {
+          Vector2 v = new Vector2(
+               x - v2.getX(),
+               y - v2.getY()
+          );
+
+          return v;
+     }
+
+     // Scales the vector
+     public Vector2 multiply(float k) {
+          Vector2 v = new Vector2(
+               k * x,
+               k * y
+          );
+
+          return v;
+     }
+
+     // Returns the normalized vector
+     public Vector2 getNormalized() {
+          float magnitude = getMagnitude();
+
+          // Return the ZERO vector if the magnitude is 0
+          if(magnitude == 0) {
+               return ZERO;
+          }
+
+          Vector2 v = new Vector2(
+               x / magnitude,
+               y / magnitude
+          );
+
+          return v;
+     }
+
+     // Rotates the vector by an angle theta (in radians) around the world centre
+     public Vector2 rotate(float theta) {
+          Vector2 v = rotateAround(Vector2.ZERO, theta);
+          return v;
+     }
+
+     public Vector2 rotateAround(Vector2 center, float theta) {
+          /* For rotating a vector around the centre, the formula is:
+
+               p' = R(θ) * (p - c) + c
+             where,
+               p' = new vector
+               R(θ) = 2D rotation matrix
+               p = vector
+               c = centre of rotation  
+          */
+
+          // Rotates the vector around a centre
+
+          float thetaRads = (float) Math.toRadians(theta);
+
+          Vector2 vectorRelativeToCenter = this.subtract(center);
+          Matrix2x2 rotationMatrix = Matrix2x2.getRotationMatrix(thetaRads);
+
+          // Adds vector 'c' to R(θ) * (p - c)
+          Vector2 rotatedVector = rotationMatrix.transform(vectorRelativeToCenter).add(center);
+
+          return rotatedVector;
+     }
+
+     // Calculates the projection of one vector on another
+     public Vector2 projectOnto(Vector2 v2) {
+          /* 
+               By formula, the projection of vector a onto b is:
+
+                    a' = k * b
+                    Where
+                         k = (a ⋅ b) / |b|²
+          */
+
+          float dotProduct = this.dot(v2);
+          float v2Mag = v2.getMagnitude();
+
+          float k = dotProduct / (v2Mag * v2Mag);
+
+          return v2.multiply(k);
+     }
+
+     // Adds two vectors
+     public static Vector2 add(Vector2 v1, Vector2 v2) {
+          return v1.add(v2);
+     }
+
+     // Subtracts two vectors
+     public static Vector2 subtract(Vector2 v1, Vector2 v2) {
+          return v1.subtract(v2);
+     }
+
+     // Gets the dot product between two vectors
+     public static float dot(Vector2 v1, Vector2 v2) {
+          return v1.dot(v2);
+     }
+
+     // Returns the 2D cross prodcut
+     public static float cross(Vector2 v1, Vector2 v2) {
+          return v1.getX() * v2.getY() - v1.getY() * v2.getX();
+     }
+
+     @Override
+     public String toString() {
+          return "<" + x + ", " + y + ">";
+     }
+
+     @Override
+     public boolean equals(Object obj) {
+          Vector2 v = (Vector2) obj;
+          return (x == v.getX()) && (y == v.getY());
+     }
+
+     public boolean isGeometricallyEqual(Vector2 other, float epsilon) {
+          if(epsilon < 0f) {
+               epsilon = 0.0001f;
+          }
+
+          return Math.abs(x - other.x) < epsilon &&
+                    Math.abs(y - other.y) < epsilon;
+     }
+}
---
+++ src/main/math/shapes/Shape.java
@@ -0,0 +1,34 @@
+package main.math.shapes;
+
+import main.math.Line;
+import main.math.algebra.Vector2;
+import main.models.data.IntersectionData;
+
+// Represents a mathemtical shape
+public abstract class Shape {
+     protected float rotation = 0f;
+
+     // Constructors
+     protected Shape() {} // Default implicit constructor
+
+     public Shape(float rotation) {
+          this.rotation = rotation;
+     }
+
+     // Getters
+     public float getRotation() {
+          return rotation;
+     }
+
+     // Setters
+     public void setRotation(float r) {
+          this.rotation = r;
+     }
+
+     // Abstract methods
+     public abstract void rotate(float theta);
+     public abstract void rotateAround(Vector2 center, float theta);
+     public abstract Line[] getEdges();
+     public abstract Line[] getWorldEdges(Vector2 parentPosition);
+     public abstract IntersectionData intersects(Line line, Vector2 parentPosition);
+}
