24cde55ed5ce9a3b014dfc6172c6055b19f6597e
Swarnim Rijal <swarnimisgone@gmail.com>
2026-08-28 19:14:41 +0545

    Added shape rendering utility

---
+++ src/main/game/ShapeRender.java
@@ -0,0 +1,218 @@
+package main.game;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
+
+import main.math.algebra.Vector2;
+import main.math.shapes.Arc;
+import main.math.shapes.Circle;
+import main.math.shapes.polygons.Polygon;
+import main.math.shapes.polygons.Rectangle;
+
+// Rendering utility for drawing shapes to the screen
+public class ShapeRender {
+
+     // Draws a circle
+     public static void draw(
+          Graphics2D g,
+          Circle circle,
+          Vector2 worldPosition,
+          float rotation,
+          Color color
+     ) {
+
+          Vector2 screenPosition = Scene.worldToScreen(worldPosition);
+
+          float radius = circle.getRadius();
+
+          AffineTransform oldTransform = g.getTransform();
+          Color oldColor = g.getColor();
+
+          g.setColor(color);
+
+          g.rotate(-rotation,
+               screenPosition.getX(),
+               screenPosition.getY()
+          );
+
+          g.fill(new Ellipse2D.Float(
+               (float) screenPosition.getX() - radius,
+               (float) screenPosition.getY() - radius,
+               radius * 2,
+               radius * 2
+          ));
+
+          g.setTransform(oldTransform);
+          g.setColor(oldColor);
+     }
+
+     // Draws a rectangle
+     public static void draw(
+          Graphics2D g,
+          Rectangle rectangle,
+          Vector2 worldPosition,
+          float rotation,
+          Color color
+     ) {
+
+          Vector2 screenPosition = Scene.worldToScreen(worldPosition);
+
+          float width = rectangle.getWidth();
+          float height = rectangle.getHeight();
+
+          AffineTransform oldTransform = g.getTransform();
+          Color oldColor = g.getColor();
+
+          g.setColor(color);
+
+          g.rotate(-rotation,
+               screenPosition.getX(),
+               screenPosition.getY()
+          );
+
+          g.fill(new Rectangle2D.Float(
+               (float) screenPosition.getX() - width / 2,
+               (float) screenPosition.getY() - height / 2,
+               width,
+               height
+          ));
+
+          g.setTransform(oldTransform);
+          g.setColor(oldColor);
+     }
+
+     // Draws an arc
+     public static void draw(
+          Graphics2D g,
+          Arc arc,
+          Vector2 worldPosition,
+          float rotation,
+          Color color
+     ) {
+
+          float radius = arc.getRadius();
+          float thickness = arc.getThickness();
+          float angle = arc.getAngle();
+
+          float innerRadius = radius - thickness;
+
+          Vector2 worldCenter =
+               arc.getWorldCenterOfCurvature(
+                    worldPosition,
+                    rotation
+               );
+
+          Vector2 screenCenter =
+               Scene.worldToScreen(worldCenter);
+
+          float cx = (float) screenCenter.getX();
+          float cy = (float) screenCenter.getY();
+
+          AffineTransform oldTransform = g.getTransform();
+          Color oldColor = g.getColor();
+
+          g.setColor(color);
+
+          Arc2D.Float outerArc =
+               new Arc2D.Float(
+                    cx - radius,
+                    cy - radius,
+                    radius * 2,
+                    radius * 2,
+                    0,
+                    (float) Math.toDegrees(angle),
+                    Arc2D.OPEN
+               );
+
+          Arc2D.Float innerArc =
+               new Arc2D.Float(
+                    cx - innerRadius,
+                    cy - innerRadius,
+                    innerRadius * 2,
+                    innerRadius * 2,
+                    (float) Math.toDegrees(angle),
+                    (float) Math.toDegrees(-angle),
+                    Arc2D.OPEN
+               );
+
+          Path2D.Float arcShape = new Path2D.Float();
+
+          arcShape.append(outerArc, false);
+          arcShape.append(innerArc, true);
+          arcShape.closePath();
+
+          g.rotate(
+               -rotation,
+               cx,
+               cy
+          );
+
+          g.fill(arcShape);
+
+          g.setTransform(oldTransform);
+          g.setColor(oldColor);
+     }
+
+     // Draws a polygon
+     public static void draw(
+          Graphics2D g,
+          Polygon polygon,
+          Vector2 worldPosition,
+          float rotation,
+          Color color
+     ) {
+
+          Vector2[] vertices = polygon.getLocalVertices();
+
+          if (vertices.length < 3)
+               return;
+
+          AffineTransform oldTransform = g.getTransform();
+          Color oldColor = g.getColor();
+
+          g.setColor(color);
+
+          Path2D.Float path = new Path2D.Float();
+
+          Vector2 first = vertices[0];
+
+          Vector2 screenFirst =
+               Scene.worldToScreen(
+                    worldPosition.add(first.rotate(rotation))
+               );
+
+          path.moveTo(
+               screenFirst.getX(),
+               screenFirst.getY()
+          );
+
+          for (int i = 1; i < vertices.length; i++) {
+
+               Vector2 vertex = vertices[i];
+
+               Vector2 screenVertex =
+                    Scene.worldToScreen(
+                         worldPosition.add(
+                              vertex.rotate(rotation)
+                         )
+                    );
+
+               path.lineTo(
+                    screenVertex.getX(),
+                    screenVertex.getY()
+               );
+          }
+
+          path.closePath();
+
+          g.fill(path);
+
+          g.setTransform(oldTransform);
+          g.setColor(oldColor);
+     }
+}
--- src/main/models/environment/reflectors/mirrors/CurvedMirror.java
+++ src/main/models/environment/reflectors/mirrors/CurvedMirror.java
@@ -2,11 +2,7 @@ package main.models.environment.reflectors.mirrors;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Path2D;
-
-import main.game.Scene;
+import main.game.ShapeRender;
 import main.math.Line;
 import main.math.algebra.Matrix2x2;
 import main.math.algebra.Vector2;
@@ -97,79 +93,15 @@ public class CurvedMirror extends Mirror {
           // Get arc collider data
           ArcCollider collider = (ArcCollider) this.getCollider();
 
-          float radius = collider.getRadius();
-          float thickness = collider.getThickness();
-          float angle = collider.getAngle();
-
-          float innerRadius = radius - thickness;
-
-
           // Get the actual center of curvature in world space
           Arc arc = (Arc) collider.getShape();
 
-          Vector2 worldCenter = arc.getWorldCenterOfCurvature(
-               this.getPosition(),
-               collider.getRotation()
+          ShapeRender.draw(
+               g,
+               arc,
+               position,
+               collider.getRotation(),
+               new Color(144, 213, 255, 190)
           );
-
-
-          // Convert world coordinates to screen coordinates
-          Vector2 screenCenter = Scene.worldToScreen(worldCenter);
-
-          float cx = screenCenter.getX();
-          float cy = screenCenter.getY();
-
-          // Save current graphics settings
-          Color oldColor = g.getColor();
-          AffineTransform oldTransform = g.getTransform();
-
-          // Set arc appearance
-          g.setColor(new Color(144, 213, 255, 190));
-
-          // Create outer circular arc
-          Arc2D.Float outerArc =
-               new Arc2D.Float(
-                    cx - radius,
-                    cy - radius,
-                    radius * 2,
-                    radius * 2,
-                    0,
-                    (float) Math.toDegrees(angle),
-                    Arc2D.OPEN
-               );
-
-          // Create inner circular arc in reverse direction
-          Arc2D.Float innerArc =
-               new Arc2D.Float(
-                    cx - innerRadius,
-                    cy - innerRadius,
-                    innerRadius * 2,
-                    innerRadius * 2,
-                    (float) Math.toDegrees(angle),
-                    (float) Math.toDegrees(-angle),
-                    Arc2D.OPEN
-               );
-
-          // Combine both arcs into a filled ring sector
-          Path2D.Float arcShape = new Path2D.Float();
-
-          arcShape.append(outerArc, false);
-          arcShape.append(innerArc, true);
-
-          arcShape.closePath();
-
-          // Rotate arc according to collider rotation
-          g.rotate(
-               -collider.getRotation(),
-               cx,
-               cy
-          );
-
-          // Render arc
-          g.fill(arcShape);
-
-          // Restore graphics state
-          g.setTransform(oldTransform);
-          g.setColor(oldColor);
      }
 }
