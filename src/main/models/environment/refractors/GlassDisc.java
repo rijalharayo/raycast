7d8641cb5be38720ca115be3f765d49c28da558d
Swarnim Rijal <swarnimisgone@gmail.com>
2026-08-29 13:15:03 +0545

    Added glass disc refractor

---
+++ src/main/models/environment/refractors/GlassDisc.java
@@ -0,0 +1,30 @@
+package main.models.environment.refractors;
+
+import main.math.algebra.Vector2;
+import main.math.shapes.Circle;
+import main.physics.colliders.CircleCollider;
+import main.physics.optics.Medium;
+
+// Represents a circular glass disc as a refractor
+public class GlassDisc extends Refractor {
+     // Constructors
+     public GlassDisc(Vector2 position, float radius) {
+          super(
+               position,
+               new CircleCollider(position, radius),
+               Medium.FLINT_GLASS,
+               new Circle(radius)
+          );
+     }
+
+     public GlassDisc(float x, float y, float radius) {
+          Vector2 pos = new Vector2(x, y);
+
+          super(
+               pos,
+               new CircleCollider(pos, radius),
+               Medium.FLINT_GLASS,
+               new Circle(radius)
+          );
+     }
+}
