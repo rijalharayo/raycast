822e78fab2f1e85863539b9d3c43ce288ed02110
Swarnim Rijal <swarnimisgone@gmail.com>
2026-08-29 21:10:56 +0545

    Added custom surface data calculation to GlassDisc

--- src/main/models/environment/refractors/glasses/GlassDisc.java
+++ src/main/models/environment/refractors/glasses/GlassDisc.java
@@ -1,7 +1,10 @@
 package main.models.environment.refractors.glasses;
 
+import main.math.algebra.Matrix2x2;
 import main.math.algebra.Vector2;
 import main.math.shapes.Circle;
+import main.models.data.IntersectionData;
+import main.models.data.SurfaceData;
 import main.models.environment.refractors.Refractor;
 import main.physics.colliders.CircleCollider;
 import main.physics.optics.Medium;
@@ -27,5 +30,27 @@ public class GlassDisc extends Refractor {
                Medium.FLINT_GLASS,
                new Circle(radius)
           );
+     }
+
+     @Override
+     public SurfaceData calculateSurfaceData(IntersectionData intersectionData) {
+          Vector2 circleCenter = this.position;
+          Vector2 intersectionPoint = intersectionData.getIntersectionPoint();
+
+          /* 
+               The radius drawn from the circle to the tangent is perpendicular to the tangent.
+               
+               Since the intersection point lies on the tanget, & also on the circumference
+               of the circle, it's at a distance of radius 'r' from the center of the
+               circle.
+
+               So the vector from the centre to the intersection-point will be the normal.
+               The same vector rotated 90deg will be the tangent line itself.
+          */
+
+          Vector2 normal = intersectionPoint.subtract(circleCenter);
+          Vector2 tangent = Matrix2x2.ROTATE_ANTI_CLOCKWISE_90.transform(normal);
+
+          return new SurfaceData(tangent, normal);
      }
 }
