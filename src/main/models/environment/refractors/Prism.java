73684b3d5acf8ad50fb8798a4f3ab5ecf78aad0a
Swarnim Rijal <swarnimisgone@gmail.com>
2026-08-29 12:45:22 +0545

    Added Glass triangular prism as a refractor

---
+++ src/main/models/environment/refractors/Prism.java
@@ -0,0 +1,45 @@
+package main.models.environment.refractors;
+
+import main.math.algebra.Vector2;
+import main.math.shapes.polygons.RightTriangle;
+import main.math.shapes.polygons.Triangle;
+import main.physics.colliders.PolygonCollider;
+import main.physics.optics.Medium;
+
+// Represents a triangular glass prism
+public class Prism extends Refractor {
+     // Constructors
+     public Prism(Vector2 position, float perpendicular, float base, float rotation) {
+          RightTriangle rightTriangle = new RightTriangle(perpendicular, base);
+
+          super(
+               position,
+               new PolygonCollider(position, rightTriangle, rotation),
+               Medium.FLINT_GLASS,
+               rightTriangle
+          );
+     }
+
+     public Prism(float x, float y, float perpendicular, float base, float rotation) {
+          RightTriangle rightTriangle = new RightTriangle(perpendicular, base);
+          Vector2 pos = new Vector2(x, y);
+
+          super(
+               pos,
+               new PolygonCollider(pos, rightTriangle, rotation),
+               Medium.FLINT_GLASS,
+               rightTriangle
+          );
+     }
+
+     public Prism(float x, float y, Triangle triangle, float rotation) {
+          Vector2 pos = new Vector2(x, y);
+          
+          super(
+               pos,
+               new PolygonCollider(pos, triangle, rotation),
+               Medium.FLINT_GLASS,
+               triangle
+          );
+     }
+}
