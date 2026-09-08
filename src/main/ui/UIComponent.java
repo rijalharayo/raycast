5bb0de9cfa3742e17d7384485f3b91539504b2df
Swarnim Rijal <swarnimisgone@gmail.com>
2026-09-08 09:43:10 +0545

    Added buttons

--- src/main/ui/UIComponent.java
+++ src/main/ui/UIComponent.java
@@ -1,33 +1,42 @@
 package main.ui;
 
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
 
+import main.input.MouseInput;
 import main.math.algebra.Vector2;
+import main.math.shapes.polygons.Rectangle;
 
 // Represents different components of the ui
 public abstract class UIComponent {
      protected Vector2 position;
-     protected int width, height;
+     protected Rectangle dimensionShape;
 
      // Constructors
      public UIComponent() {}
 
      public UIComponent(float x, float y, int width, int height) {
           this.position = new Vector2(x, y);
-          this.width = width;
-          this.height = height;
+          this.dimensionShape = new Rectangle(width, height);
      }
 
      public UIComponent(int width, int height) {
           this.position = Vector2.ZERO;
-          this.width = width;
-          this.height = height;
+          this.dimensionShape = new Rectangle(width, height);
      }
 
      public UIComponent(float x, float y) {
           this.position = new Vector2(x, y);
-          this.width = 1;
-          this.height = 1;
+          this.dimensionShape = new Rectangle(1, 1);
+     }
+
+     // Getters
+     protected int width() {
+          return dimensionShape.getWidth();
+     }
+
+     protected int height() {
+          return dimensionShape.getHeight();
      }
 
      // Setters
@@ -36,11 +45,37 @@ public abstract class UIComponent {
      }
 
      public void setSize(int width, int height) {
-          this.width = width;
-          this.height = height;
+          this.dimensionShape = new Rectangle(width, height);
      }
+
+     // Updates components
+     public void update() {
+          if(dimensionShape != null) {
+               if(dimensionShape.containsPoint(position, MouseInput.getMousePosition())) {
+                    onHover();
+                    if(MouseInput.isPressed(MouseEvent.BUTTON1)) {
+                         onClick();
+                    }
+               }
+               else {
+                    setAsDefault();
+               }
+          }
+     };
+
+     protected void onHover() {
+          // Do nothing
+     }
+
+     protected void onClick() {
+          // Do nothing
+     }
+
+     protected void setAsDefault() {
+          // Do nothing
+     };
 
      public abstract void render(Graphics2D g);
      // Renders at some other position
-     public abstract void render(Graphics2D g, Vector2 position);
+     public void render(Graphics2D g, Vector2 position) {};
 }
---
+++ src/main/ui/components/UIButton.java
@@ -0,0 +1,115 @@
+package main.ui.components;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+
+import java.awt.Graphics2D;
+
+import main.game.Scene;
+import main.math.algebra.Vector2;
+import main.ui.Fonts;
+import main.ui.UIComponent;
+
+// Buttons
+public class UIButton extends UIComponent {
+     public static final Font DEFAULT_FONT = Fonts.loadFont();
+     private static final Color DEFAULT_BUTTON_COLOR = new Color(28, 27, 29); // Dark grey-ish color
+
+     private UIText uiText;
+     private float borderWidth = 10f;
+     private Color buttonColor = DEFAULT_BUTTON_COLOR; // Dark grey-ish color
+     private Color borderColor = new Color(59, 59, 59); // Lighter grey-ish color
+
+     private Color hoverColor = new Color(41, 41, 41); // A little lighter grey-ish color than buttonColor
+     private Color clickColor = new Color(31, 31, 31); // A little darker than hover color
+
+     private Runnable onClick;
+
+     // Constructors
+     public UIButton(String text, int width, int height) {
+          super(width, height);
+
+          this.uiText = new UIText(text, 20f);
+          uiText.setColor(Color.WHITE);
+     }
+
+     // Setters
+
+     public void setText(String txt) {
+          uiText.setText(txt);
+     }
+
+     public void setTextSize(float sz) {
+          uiText.setSize(sz);
+     }
+
+     public void setButtonColors(Color mainColor, Color borderColor) {
+          this.borderColor = borderColor;
+          this.buttonColor = mainColor;
+     }
+
+     public void setOnClick(Runnable action) {
+          this.onClick = action;
+     }
+
+     @Override
+     protected void onHover() {
+          if(!buttonColor.equals(hoverColor)) {
+               buttonColor = hoverColor;
+          }
+     }
+
+     @Override
+     protected void onClick() {
+          // Flicker the click color when clicked
+          if(!buttonColor.equals(clickColor)) {
+               buttonColor = clickColor;
+          }
+
+          // Trigger click event
+          click();
+     }
+
+     @Override
+     protected void setAsDefault() {
+          if(!buttonColor.equals(DEFAULT_BUTTON_COLOR)) {
+               buttonColor = DEFAULT_BUTTON_COLOR;
+          }
+     }
+
+     // Does action when clicked
+     public void click() {
+          if (onClick != null) {
+               onClick.run();
+          }
+     }
+
+     @Override
+     public void render(Graphics2D g) {
+          Vector2 screenPos = Scene.worldToScreen(position);
+
+          float x = screenPos.getX() - (width() / 2f);
+          float y = screenPos.getY() - (height() / 2f);
+
+          // Button
+          g.setColor(buttonColor);
+          g.fillRect((int) x, (int) y, width(), height());
+
+          // Border
+          g.setColor(borderColor);
+          g.setStroke(new BasicStroke(borderWidth));
+
+          float borderOffset = borderWidth / 2f;
+
+          g.drawRect(
+               (int) (x + borderOffset),
+               (int) (y + borderOffset),
+               (int) (width() - borderWidth),
+               (int) (height() - borderWidth)
+          );
+
+          // Text
+          uiText.render(g, position);
+     }
+}
