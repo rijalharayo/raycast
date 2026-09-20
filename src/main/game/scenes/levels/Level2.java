cf4714cd1a72c0a984569dc7d1cc5846c7be5f17
Swarnim Rijal <swarnimisgone@gmail.com>
2026-09-20 22:50:35 +0545

    Created level 2

--- src/main/game/scenes/levels/Level2.java
+++ src/main/game/scenes/levels/Level2.java
@@ -2,6 +2,9 @@ package main.game.scenes.levels;
 
 import main.game.scenes.LevelScene;
 import main.models.entities.Laser;
+import main.models.environment.TargetEnergyOrb;
+import main.models.environment.absorbers.BlackAbsorber;
+import main.models.environment.reflectors.mirrors.PlaneMirror;
 
 public class Level2 extends LevelScene {
      public Level2() {
@@ -10,6 +13,20 @@ public class Level2 extends LevelScene {
 
      @Override
      public void loadObjects() {
-          add(new Laser(100f, 100f));
+          add(new Laser(500, 300));
+
+          // Mirror 1
+          PlaneMirror mirror1 = new PlaneMirror(-634.5f, 305f, 200, 20, 45f);
+          mirror1.setDraggable(false);
+          add(mirror1);
+
+          // Mirror 2 (movable)
+          add(new PlaneMirror(100, 100, 200, 20, -45f));
+
+          BlackAbsorber absorber = new BlackAbsorber(-200f, -150f, 20, 600, 0f);
+          absorber.setDraggable(false);
+          add(absorber);
+
+          add(new TargetEnergyOrb(-300, -300, 30f));
      }
 }
